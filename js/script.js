9f97befa3be939bc3cbaf05986f7c86b878e3921
adrianortzs <adrianortizsuarez@icloud.com>
2024-02-23 22:06:50 +0100

    ejercicio resuelto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
  // Aquí tu código
+const lista = document.getElementById("lista")
+const button = document.getElementById("agregar")
+
+button.addEventListener("click",  function addSomething() {
+    const something = prompt("Añade algo a la lista")
+    if (something){
+        const create = document.createElement("li")
+        create.textContent = something
+        lista.appendChild(create)
+    }
+})
